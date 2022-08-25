e5f8457e93c5fe722d16b7da65c7cf6b8a4203eb
FaceDeer <derksenmobile@gmail.com>
2022-08-24 20:41:38 -0600

    better creative check

--- features.lua
+++ features.lua
@@ -43,7 +43,7 @@ local stal_on_place = function(itemstack, placer, pointed_thing)
 
 	-- add the node and remove 1 item from the itemstack
 	minetest.add_node(pt.above, {name = itemstack:get_name(), param2 = new_param2})
-	if not minetest.settings:get_bool("creative_mode", false) then
+	if not minetest.is_creative_enabled(placer:get_player_name()) then
 		itemstack:take_item()
 	end
 	return itemstack
